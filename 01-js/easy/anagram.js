5c7310c4b68f897f8ede8c83cb04a32a35d6ca10
panchalmilan <mpanchal1310@gmail.com>
2023-06-15 15:38:27 +0530

    solve anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,32 @@
 */
 
 function isAnagram(str1, str2) {
+  const len1 = str1.length;
+  const len2 = str2.length;
+  if (len1 !== len2) return false;
 
+  const hashmap = {};
+  // count frequency of chars from string 1
+  for (let i = 0; i < len1; i++) {
+    const ch = str1[i].toLowerCase();
+    if (ch in hashmap) hashmap[ch]++;
+    else hashmap[ch] = 1;
+  }
+
+  // delete frequency of chars from string 2
+  for (let i = 0; i < len2; i++) {
+    const ch = str2[i].toLowerCase();
+    if (ch in hashmap) hashmap[ch]--;
+    else return false; // char is not present in string 1
+  }
+
+  // check frequency of chars
+  for (const ch in hashmap) {
+    const freq = hashmap[ch];
+    if (freq > 0) return false; // if freq of a char in str2 is greater than str1
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
