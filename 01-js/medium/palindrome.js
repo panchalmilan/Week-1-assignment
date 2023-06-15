d137690001a95183d962a17fc984cdd81a31dcac
panchalmilan <mpanchal1310@gmail.com>
2023-06-15 16:26:19 +0530

    solve palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -6,7 +6,24 @@
   - `npm run test-palindrome`
 */
 
+const a_ASCII = 97;
+const z_ASCII = 122;
+
 function isPalindrome(str) {
+  const s = str.toLowerCase();
+  let formattedStr = '';
+  for (let i = 0; i < s.length; i++) {
+    const ascii = s.charCodeAt(i);
+    if (a_ASCII <= ascii && ascii <= z_ASCII)
+      formattedStr = formattedStr + s[i];
+  }
+  const n = formattedStr.length;
+
+  for (let i = 0; i < n / 2; i++) {
+    const elem = formattedStr[i];
+    const mirrorElem = formattedStr[n - i - 1];
+    if (elem !== mirrorElem) return false;
+  }
   return true;
 }
 
