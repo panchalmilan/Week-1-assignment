f3e1e324d4b340d06f055a4df5ef7c04cf4edcb1
panchalmilan <mpanchal1310@gmail.com>
2023-06-15 16:56:52 +0530

    solve todolist

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,51 @@
 */
 
 class Todo {
+  constructor() {
+    this.todoList = [];
+  }
 
+  isValidIndex(idx) {
+    return 0 <= idx && idx < this.todoList.length;
+  }
+
+  add(todo) {
+    this.todoList.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    if (!this.isValidIndex(indexOfTodo)) return;
+    this.todoList.splice(indexOfTodo, 1);
+  }
+
+  update(index, updatedTodo) {
+    if (!this.isValidIndex(index)) return;
+    this.todoList[index] = updatedTodo;
+  }
+
+  getAll() {
+    return this.todoList;
+  }
+
+  get(indexOfTodo) {
+    if (!this.isValidIndex(indexOfTodo)) return null;
+    return this.todoList[indexOfTodo];
+  }
+
+  clear() {
+    this.todoList = [];
+  }
 }
+
+const todo = new Todo();
+
+todo.add('todo1');
+todo.add('todo2');
+todo.add('todo3');
+todo.add('todo4');
+todo.remove(1);
+console.log(todo.getAll());
+todo.update(1, 'todonew');
+console.log(todo.getAll());
 
 module.exports = Todo;
